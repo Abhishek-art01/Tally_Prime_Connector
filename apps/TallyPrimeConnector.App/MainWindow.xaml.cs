b3d999a00892cf87b3e9d8efb2203bf11713c496
Abhishek Pandey <ap0808661@gmail.com>
2026-09-16 15:48:08 +0530

    Use native Windows title bar

--- apps/TallyPrimeConnector.App/MainWindow.xaml.cs
+++ apps/TallyPrimeConnector.App/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
 
 namespace TallyPrimeConnector.App;
 
@@ -13,44 +10,4 @@ public partial class MainWindow : Window
         DataContext = viewModel;
     }
 
-    private void TitleBar_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs eventArgs)
-    {
-        if (eventArgs.OriginalSource is DependencyObject source && FindVisualParent<Button>(source) is not null)
-        {
-            return;
-        }
-
-        if (eventArgs.ClickCount == 2)
-        {
-            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
-            return;
-        }
-
-        try
-        {
-            DragMove();
-        }
-        catch (InvalidOperationException)
-        {
-            // The pointer was released before WPF began the drag operation.
-        }
-    }
-
-    private void Minimize_OnClick(object sender, RoutedEventArgs eventArgs) => WindowState = WindowState.Minimized;
-
-    private void Maximize_OnClick(object sender, RoutedEventArgs eventArgs) =>
-        WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
-
-    private static T? FindVisualParent<T>(DependencyObject source) where T : DependencyObject
-    {
-        for (var current = source; current is not null; current = VisualTreeHelper.GetParent(current))
-        {
-            if (current is T matching)
-            {
-                return matching;
-            }
-        }
-
-        return null;
-    }
 }
